dfed0f3a86ad0410faae990891cb06087bebe5eb
orenshaf <orenshaf@gmail.com>
2019-06-18 23:27:27 -0400

    gifs for loading map loads on time

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import FetchData from '../lib/data/fetch_data';
 
 document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(() => document.getElementById("loading").remove(), 16000)
     FetchData();
 })
 
