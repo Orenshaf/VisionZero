40d5151743d7221a72704a54a0ed924a1433cd6c
orenshaf <orenshaf@gmail.com>
2019-06-19 11:03:44 -0400

    heatmap works

--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
 import FetchData from '../lib/data/fetch_data';
 
 document.addEventListener("DOMContentLoaded", () => {
-    setTimeout(() => {
-        document.getElementById("loading").remove()
-        const dashboard = document.getElementById("dashboard");
-        dashboard.removeAttribute("style");
-    }, 16000)
     FetchData();
 })
 
